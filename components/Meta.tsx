1cfe49b6f5cd008c4e52458257094e44f04e1ac4
Mathurah Ravigulan <mathurah25@gmail.com>
2022-09-18 12:43:03 -0700

    Added google analytics tags

--- components/Meta.tsx
+++ components/Meta.tsx
@@ -1,15 +1,39 @@
-import Head from "next/head"
+import Head from "next/head";
+import Script from "next/script";
 
 export const Meta = () => {
   return (
-    <Head>
-      <title>Loo Purity Test</title>
-      <meta name="title" content="Loo Purity Test" />
-      <meta name="description" content="Hi disciple of Mr. Goose, how pure are you? take this test to find out." />
-      <link rel="icon" href="/favicon.ico" />
-      <meta property="og:type" content="website" />
-      <meta name="og:title" content="Loo Purity Test" />
-      <meta name="og:description" content="Hi disciple of Mr. Goose, how pure are you? take this test to find out." />
-    </Head>
-  )
-}
+    <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+
+      <Script strategy="lazyOnload">
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+        page_path: window.location.pathname,
+        });
+    `}
+      </Script>
+      <Head>
+        <title>Loo Purity Test</title>
+        <meta name="title" content="Loo Purity Test" />
+        <meta
+          name="description"
+          content="Hi disciple of Mr. Goose, how pure are you? take this test to find out."
+        />
+        <link rel="icon" href="/favicon.ico" />
+        <meta property="og:type" content="website" />
+        <meta name="og:title" content="Loo Purity Test" />
+        <meta
+          name="og:description"
+          content="Hi disciple of Mr. Goose, how pure are you? take this test to find out."
+        />
+      </Head>
+    </>
+  );
+};
