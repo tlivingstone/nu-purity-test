74bd0a72210d9620a128af913d5fddaaf0724e92
straightupjac <jdc.jaclyn@gmail.com>
2022-11-18 15:40:39 -0800

    fix: image

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,12 @@ const Home: NextPage = () => {
   return (
     <div className={styles.container}>
       <main className={styles.main}>
-        <Image src="/official-logo.png" alt="Loo purity test logo" />
+        <Image
+          src="/official-logo.png"
+          alt="Loo purity test logo"
+          width={1000}
+          height={200}
+        />
         {/* <h1 className={styles.title}>Welcome to the Waterloo Purity Test</h1> */}
         <p className={styles.description}>
           Caution: This is not a bucket list. Completion of all items on this
