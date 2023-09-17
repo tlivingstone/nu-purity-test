c404e32dea578d8d0608b8c11e8766ebfcdc89c0
Taryn Livingstone <livingstonetaryn@gmail.com>
2023-09-17 11:49:30 -0400

    first commit

--- components/Meta.tsx
+++ components/Meta.tsx
@@ -20,18 +20,18 @@ export const Meta = () => {
     `}
       </Script>
       <Head>
-        <title>Loo Purity Test</title>
-        <meta name="title" content="Loo Purity Test" />
+        <title>NU Purity Test</title>
+        <meta name="title" content="NU Purity Test" />
         <meta
           name="description"
-          content="Hi disciple of Mr. Goose, how pure are you? take this test to find out."
+          content="hi disciple of Paws, how pure are you? take this test to find out."
         />
         <link rel="icon" href="/favicon.ico" />
         <meta property="og:type" content="website" />
-        <meta name="og:title" content="Loo Purity Test" />
+        <meta name="og:title" content="NU Purity Test" />
         <meta
           name="og:description"
-          content="Hi disciple of Mr. Goose, how pure are you? take this test to find out."
+          content="hi disciple of Paws, how pure are you? take this test to find out."
         />
       </Head>
     </>
--- components/footer.tsx
+++ components/footer.tsx
@@ -13,9 +13,11 @@ const Footer = () => {
           </a>
         </Text>
         <Text>
-          not affiliated with the university of waterloo &copy;{" "}
+          not affiliated with northestern university &copy;{" "}
           {new Date().getFullYear()} | we do not collect any identifiable data |
-          if you have questions for us, show yourself on r/uwaterloo
+          if you have questions for us, show yourself on{" "}
+          <a href="https://www.reddit.com/r/NEU/">r/neu</a>
+          {/* how do i get rid of the space here? */}
         </Text>
       </VStack>
     </footer>
